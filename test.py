34fb655278c6a3387436d2fd30f7bdb7f4398f5c
eyhui <ethanlovedoge@gmail.com>
2024-09-09 10:51:25 -0500

    i did some change

--- test.py
+++ test.py
@@ -1,1 +1,55 @@
 print("hello world")
+print("the following quote is from a book i read for some reason")
+
+x = 0
+for x in range(5):
+    print(x+1)
+    x+=1
+
+'''
+To those who fight and rage against superstition, I say only this: why? Why admit to such pride and vanity that you carelessly disregard years of tradition? When the peasant tells you that the great mountains of ancient China were destroyed by the angry gods, that thousands of years ago the skies were torn down, the country set out of kilter, why not believe him? Are you so much cleverer than he is? Are you cleverer than all his generations taken together?
+
+I believe him. Now, at last, I believe. I tremble to write it, but I do, I believe all that superstition tells us. And why? Because there is nothing else to explain the vagaries of this world, no other tool to translate this disaster. So I turn to folklore for my comfort, and I trust the peasant when he says that the wrath of the gods has caused the land to slope downwards to the east. Yes, I trust him when he tells me that everything, river, mud and towns, must eventually slide into the sea. Nanking too. One day Nanking too will slide away to the sea. Her journey may be the slowest, for she is no longer quite like other cities. These last few days have changed her beyond recognition and when she begins to move it will be slowly, for she is tethered to the land by her unburied citizens, and by the ghosts that will pursue her to the coast and back.
+
+Maybe I should consider myself privileged to see her as she is now. From this tiny window I can peer out through the lattice and see what the Japanese have left of her: her blackened buildings, the empty streets, the corpses piling up in the canals and rivers. Then I look down at my shaking hands and wonder why I have survived. The blood is dry now. If I rub my palms together it flakes off, the black scales scatter on the paper, darker than the words I write because my ink is watery: the pine soot inkstick is finished and I haven’t the strength or the courage or the will to go out and find more.
+
+If I were to lay down my pen, lean sideways against the cold wall and adopt an awkward position with my nose squashed against the shutters, I would be able to see Purple Mountain, snow-covered, rising up beyond the shattered roofs. But I will not. There is no call to push my body into an unnatural place because I will never again look upon Purple Mountain. When this diary entry is finished I will have no desire to recall myself, up on those slopes, a ragged and uneven figure, keeping desperate pace with the Japanese soldier, tracking him like a wolf, through frozen streams and snowdrifts . . .
+
+It is less than two hours. Two hours since I caught up with him. We were in a small grove near the mausoleum gates. He was standing with his back to me next to a tree, the melting snow in the branches dripping down on to his shoulders. His head was bent forward a little to peer into the forest ahead, because the mountain slopes are still a dangerous place to be. The cine-camera dangled at his side.
+
+I had been following him for so long that I was bruised and limping, my lungs stinging in the cold air. I came forward slowly. I can’t, now, imagine how I was able to remain so controlled because I was trembling from head to toe. When he heard me he whirled round, falling instinctively to a crouch. But I am not much of a man, not strong, and a full head shorter than he was, and when he saw it was me, he relaxed a little. He straightened slowly, watching me come a few steps nearer until we were only feet apart, and he could see the tears on my face.
+
+‘It will mean nothing to you,’ he said, with something like pity in his voice, ‘but I want you to know that I am sorry. I am very sorry. Do you understand my Japanese?’
+
+‘Yes, I do.’
+
+He sighed and rubbed his forehead with his cracked pigskin glove. ‘It wasn’t as I would have wished it. It never is. Please believe this.’ He raised his hand in the vague direction of the Linggu Temple. ‘It is true that – that he enjoyed it. He always does. But I don’t. I watch them. I make films of what they do, but I take no pleasure from it. Please trust me in this, I take no pleasure .’
+
+I wiped my face with my sleeve, pushing away the tears. I stepped forward and put a trembling hand on his shoulder. He didn’t flinch – he stood his ground, searching my face, puzzled. There was no fear in his expression: he thought of me as a defenceless civilian. He knew nothing about the small fruit knife hidden in my hand.
+
+‘Give me the camera,’ I said.
+
+‘I can’t. Don’t believe I make these films for their recreation, for the soldiers. I have far greater intentions than that.’
+
+‘Give me the camera.’
+
+He shook his head. ‘Absolutely not.’
+
+With those words the world around us seemed to me to slow down abruptly. Somewhere on the distant slopes below, the Japanese sampohei artillery were laying down heavy mortar fire, chasing renegade Nationalist units off the mountains, rounding them up and forcing them back down to the city, but up on the higher slopes I was aware of no sound at all, save the thudding of our hearts, the ice melting in the trees around us.
+
+‘I said give me the camera.’
+
+‘And I repeat no. Absolutely not.’
+
+I opened my mouth then, canted forward a little and released a howl directly into his mouth. It had been building in me all the time I’d been chasing him through the snow, and now I screamed, like a wounded animal. I lunged, twisting the little knife into him, through the khaki uniform, grinding through the lucky senninbari belt. He didn’t make a sound. His face moved, his head jerked up so fast that his army cap fell off, and we both stumbled back a pace in surprise, staring down at what I’d done. Gouts of blood fell into the snow and the inside of his stomach folded out like creamy fruit through the rip in his uniform. He stared at it for a moment, as if puzzled. Then the pain reached him. He dropped his rifle and grabbed at his stomach, trying to push it back inside. ‘ Kuso! ’ he said. ‘What have you done?’
+
+I staggered back, dropping the knife into the snow, groping blindly for a tree to lean against. The soldier turned away from me and lurched into the forest. One hand clutching his belly, the other still holding the camera, he went unsteadily, his head held up with a peculiar dignity, as if he was heading somewhere important, as if a better, safer world lay somewhere out in the trees. I went after him, stumbling in the snow, my breath coming fast and hot. After about ten yards he tripped, almost lost his balance and cried out something: a woman’s name in Japanese, his mother’s maybe, or his wife’s. He raised his arm and the movement must have loosened things inside because some dark and long part of him slithered out of the wound, dropping into the snow. He slipped in it and tried to regain his balance, but now he was very weak and he could only stagger, in a hazy circle, a long red cord trailing from him, as if this was a birth and not a death.
+
+‘Give it to me. Give me the camera.’
+
+He couldn’t answer. He had lost all ability to reason: he was no longer aware of what was happening. He sank to his knees, his arms raised slightly, and rolled softly on to his side. I was next to him in a second. His lips were blue and there was blood coating his teeth. ‘No,’ he whispered, as I prised his gloved fingers from the camera. His eyes were already blind, but he could sense where I was. He groped for my face. ‘Don’t take it. If you take it who will tell the world?’
+
+If you take it who will tell the world?
+
+Those words have stayed with me. They will be with me for the rest of my life. Who will tell? I stare for a long time at the sky above the house, at the black smoke drifting across the moon. Who will tell? The answer is, no one. No one will tell. It is all over. This will be the last entry in my journal. I will never write again. The rest of my story will stay on the film inside the camera, and what happened today will remain a secret.
+'''
